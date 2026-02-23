f554404fad28b3c0ce458193e4a3078c077c06b1
hr5046 <hr5046@srmist.edu.in>
2026-02-23 09:00:37 +0530

    Initial commit

---
+++ OOPS_ASCII_II.java
@@ -0,0 +1,35 @@
+class OOPS_ASCII_II {
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                " ***** ", " ***** ", " ****** ", " ***** "
+        ));
+
+        System.out.println(String.join(" ",
+                " ** ** ", " ** ** ", " **    ", " **    "
+        ));
+
+        System.out.println(String.join(" ",
+                " ** ** ", " ** ** ", " **    ", " **    "
+        ));
+
+        System.out.println(String.join(" ",
+                " ** ** ", " ** ** ", " ****** ", " ***   "
+        ));
+
+        System.out.println(String.join(" ",
+                " ** ** ", " ** ** ", " **     ", "   **  "
+        ));
+
+        System.out.println(String.join(" ",
+                " ** ** ", " ** ** ", " **     ", "   **  "
+        ));
+
+        System.out.println(String.join(" ",
+                " ***** ", " ***** ", " **     ", " ***** "
+        ));
+    }
+}
+
+
+    
