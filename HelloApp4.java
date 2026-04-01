7691045fd25bd878fbad5310c8129e68269902eb
hr5046 <hr5046@srmist.edu.in>
2026-04-01 23:04:22 +0530

    UC

---
+++ HelloApp4.java
@@ -0,0 +1,24 @@
+public class HelloApp4 {
+
+    public static void main(String[] args) {
+
+        String name = "World";
+
+        if (args.length > 0) {
+
+            StringBuilder nameBuilder = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+
+            name = nameBuilder.toString();
+        }
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
